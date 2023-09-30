b80393b3141b464de7840928067db488ab853785
Ivano PETTY <96083465+Ivano-P@users.noreply.github.com>
2023-09-30 12:11:35 +0200

    added first backend microservice ms_gestionPatient, this microservice is not yet codded, just initialized

---
+++ ms_gestionPatient/src/main/java/com/medilabosolutions/ms_gestionPatient/MsGestionPatientApplication.java
@@ -0,0 +1,13 @@
+package com.medilabosolutions.ms_gestionPatient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsGestionPatientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsGestionPatientApplication.class, args);
+	}
+
+}
---
+++ ms_gestionPatient/src/test/java/com/medilabosolutions/ms_gestionPatient/MsGestionPatientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medilabosolutions.ms_gestionPatient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsGestionPatientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
