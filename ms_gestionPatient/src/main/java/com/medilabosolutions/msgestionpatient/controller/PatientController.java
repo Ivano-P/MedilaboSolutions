bd9e33e3b27ec180ca7e8fd63d19e1ede06c2420
Ivano PETTY <96083465+Ivano-P@users.noreply.github.com>
2023-10-02 21:04:38 +0200

    Update PatientController.java

--- ms_gestionPatient/src/main/java/com/medilabosolutions/msgestionpatient/controller/PatientController.java
+++ ms_gestionPatient/src/main/java/com/medilabosolutions/msgestionpatient/controller/PatientController.java
@@ -2,10 +2,8 @@ package com.medilabosolutions.msgestionpatient.controller;
 
 
 import com.medilabosolutions.msgestionpatient.dto.PatientDTO;
-import com.medilabosolutions.msgestionpatient.model.Patient;
 import com.medilabosolutions.msgestionpatient.service.PatientService;
 import lombok.AllArgsConstructor;
-import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,7 +11,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import java.util.List;
 
 
-@Log4j2
 @AllArgsConstructor(onConstructor = @__(@Autowired))
 @Controller
 public class PatientController {
