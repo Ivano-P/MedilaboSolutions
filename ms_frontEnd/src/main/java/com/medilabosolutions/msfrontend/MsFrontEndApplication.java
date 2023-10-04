47305bedf0708098dd7989759aaba9a3c85f4864
Ivano PETTY <96083465+Ivano-P@users.noreply.github.com>
2023-10-04 11:09:35 +0200

    -added front-end microservice module to project

---
+++ ms_frontEnd/src/main/java/com/medilabosolutions/msfrontend/MsFrontEndApplication.java
@@ -0,0 +1,13 @@
+package com.medilabosolutions.msfrontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsFrontEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsFrontEndApplication.class, args);
+	}
+
+}
---
+++ ms_frontEnd/src/test/java/com/medilabosolutions/msfrontend/MsFrontEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medilabosolutions.msfrontend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsFrontEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
