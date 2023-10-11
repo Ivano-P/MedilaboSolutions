5e3f6cfb1f4c3d56ea7c867471a83a37ba2fa18c
Ivano PETTY <96083465+Ivano-P@users.noreply.github.com>
2023-10-11 12:33:24 +0200

    re added spring security to gateway but deactivated blocking for now

--- ms_gestionPatients/src/main/java/com/medilabosolutions/msgestionpatient/MsGestionPatientApplication.java
+++ ms_gestionPatients/src/main/java/com/medilabosolutions/msgestionpatient/MsGestionPatientApplication.java
@@ -8,6 +8,8 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import java.util.List;
 
@@ -18,6 +20,11 @@ public class MsGestionPatientApplication{
 
 	public static void main(String[] args) {
 		SpringApplication.run(MsGestionPatientApplication.class, args);
+	}
+
+	@Bean
+	public BCryptPasswordEncoder bCryptPasswordEncoder() {
+		return new BCryptPasswordEncoder();
 	}
 
 }
