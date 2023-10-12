a20b06b605b834e556c34b0d35c6cb2348051cd6
Ivano PETTY <96083465+Ivano-P@users.noreply.github.com>
2023-10-12 10:48:58 +0200

    added ms_gestionHistorique microservice to project, start of sprint 2

---
+++ ms_gestionHistorique/src/main/java/com/medilabosolutions/msgestionhistorique/MsGestionHistoriqueApplication.java
@@ -0,0 +1,13 @@
+package com.medilabosolutions.msgestionhistorique;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsGestionHistoriqueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsGestionHistoriqueApplication.class, args);
+	}
+
+}
---
+++ ms_gestionHistorique/src/test/java/com/medilabosolutions/msgestionhistorique/MsGestionHistoriqueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medilabosolutions.msgestionhistorique;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsGestionHistoriqueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
